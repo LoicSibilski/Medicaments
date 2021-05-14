bdf4870530bfebefa00880e842690e497ef3889c
SamyMercier <56022459+SamyMercier@users.noreply.github.com>
2021-05-14 14:00:31 +0200

    Update user.ts

--- angular-medicament/src/app/feature/users/models/user.ts
+++ angular-medicament/src/app/feature/users/models/user.ts
@@ -1,6 +1,40 @@
+import { Medic } from "../../medics/models/medic";
+import { Notification } from "../../notification/models/notification";
+import { Rendezvous } from "../../rendezvous/models/rendezvous";
+
 export class User {
+    public compteId : number;
+    public utilisateurId : number;
+    public nom : string;
+    public prenom : string;
+    public dateDeNaissance : Date;
+    public assistants : User[];
+    public assistes : User[];
+    public medicaments : Medic[];
+    public notifications : Notification[];
+    public rendezVous : Rendezvous[];
 
-    
-
-
+    constructor(
+        compteId : number,
+        utilisateurId : number,
+        nom : string,
+        prenom : string,
+        dateDeNaissance : Date,
+        assistants : User[],
+        assistes : User[],
+        medicaments : Medic[],
+        notifications : Notification[],
+        rendezVous : Rendezvous[]
+        ) {
+            this.compteId = compteId;
+            this.utilisateurId = utilisateurId;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.dateDeNaissance = dateDeNaissance;
+            this.assistants = assistants;
+            this.assistes = assistes;
+            this.medicaments = medicaments;
+            this.notifications = notifications;
+            this.rendezVous = rendezVous;
+    }
 }
