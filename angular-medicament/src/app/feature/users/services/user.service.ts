3b1aa43a1d02148e549af720b2a909f8ece03c4d
Loic Sibilski <aeneas62@hotmail.fr>
2021-05-14 16:45:21 +0200

    ajout de la branch prescription

--- angular-medicament/src/app/feature/users/services/user.service.ts
+++ angular-medicament/src/app/feature/users/services/user.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Medic } from '../../medics/models/medic';
 import { Prescription } from '../../prescription/models/prescription';
