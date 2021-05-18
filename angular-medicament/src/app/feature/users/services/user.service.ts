423c1e93ceff07ecf0ba9c42449a769bb8956266
Loic Sibilski <aeneas62@hotmail.fr>
2021-05-18 10:42:51 +0200

    feat(user): maj des objets prescriptions dans le user.service

--- angular-medicament/src/app/feature/users/services/user.service.ts
+++ angular-medicament/src/app/feature/users/services/user.service.ts
@@ -8,44 +8,6 @@ import { User } from '../models/user';
 })
 export class UserService {
 
-  prescrDoli1 : Prescription ={
-    'dateDebut' : new Date('20/05/1992'),
-    'dateFin' : new Date('27/05/1998'),
-    'nombreUnite' : 2,
-    'isMatin' : true,
-    'isMidi' : true, 
-    'isSoir' : false,
-  }
-  prescrMemorex1 : Prescription ={
-    'dateDebut' : new Date('25/06/2014'),
-    'dateFin' : new Date('01/01/2021'),
-    'nombreUnite' : 4,
-    'isMatin' : false,
-    'isMidi' : false, 
-    'isSoir' : true,
-  }
-  prescrMemorex2 : Prescription ={
-    'dateDebut' : new Date('10/05/1995'),
-    'dateFin' : new Date('14/05/2025'),
-    'nombreUnite' : 4,
-    'isMatin' : false,
-    'isMidi' : false, 
-    'isSoir' : true,
-  }
-  doliprane : Medic = {
-    'nom': 'Doliprane',
-    'prescription' : [
-      this.prescrDoli1
-    ]
-  }
-
-  memorex : Medic = {
-    'nom': 'Memorex',
-    'prescription' : [
-      this.prescrMemorex1,
-      this.prescrMemorex2,
-    ]
-  }
 
   constructor() { }
 
