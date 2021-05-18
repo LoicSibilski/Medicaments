66796da4fb0a225ae9e40f217cf459235b5a7b15
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-05-18 16:48:11 +0200

    Ajout

--- angular-medicament/src/app/feature/compte/models/compte.ts
+++ angular-medicament/src/app/feature/compte/models/compte.ts
@@ -1,2 +1,32 @@
+import { User } from "../../users/models/user";
+
 export class Compte {
+    private _id: number;
+    private _email: string;
+    private _password: string;
+    private _users: User[];
+
+    constructor(id: number, email: string, password: string, users: User[]) {
+        this._id = id;
+        this._email = email;
+        this._password = password;
+        this._users = users;
+    }
+
+    // Getter 
+    get id () : number {
+        return this._id;
+    }
+
+    // Setter
+    set id (id:number) {
+        this._id = id;
+    }
+
+
+    get email () : string {
+        return this._email;
+    }
+
+    
 }
