92cca127664a332bfa5b08f8f228090cb2e2f04b
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-05-19 11:46:01 +0200

    Ajout d'un objet compte

--- angular-medicament/src/app/feature/compte/services/compte.service.ts
+++ angular-medicament/src/app/feature/compte/services/compte.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Compte } from '../models/compte';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompteService {
 
+  // Instancier un objet de type compte
+  compte : Compte = new Compte(1,"moniquedubois@example.com","Du1950!bois", []);
+
   constructor() { }
+
 }
