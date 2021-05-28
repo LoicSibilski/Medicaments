dd32002e9b3f70e11f90b10ea35f98465a7bfc5e
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-05-28 17:21:35 +0200

    Suppression commentaire

--- angular-medicament/src/app/feature/compte/services/compte.service.ts
+++ angular-medicament/src/app/feature/compte/services/compte.service.ts
@@ -11,10 +11,6 @@ import { Compte } from '../models/compte';
  */
 export class CompteService {
 
-  // TODO
-  // matcher l'adresse mail et le mot de passe, l'adresse mail est unique, le mot de passe peut exister sur des comptes différents.
-  // capturer et gérer l'erreur, si une donnée est null 
-
   compte1 : Compte = new Compte(1,"test1@test.com","test1", []); 
   compte2 : Compte = new Compte(2,"test2@test.com","test2", []);
 
