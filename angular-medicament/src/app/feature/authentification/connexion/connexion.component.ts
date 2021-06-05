73f853f742e2a3f63e637347ce70fec8ff4f419e
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-05 18:51:11 +0200

    remise à zéro

--- angular-medicament/src/app/feature/authentification/connexion/connexion.component.ts
+++ angular-medicament/src/app/feature/authentification/connexion/connexion.component.ts
@@ -15,25 +15,8 @@ import { Compte } from '../../compte/models/compte';
  */
 export class ConnexionComponent implements OnInit {
 
-  // TODO 
-  // récupérer les données du formulaire
-  // vérifier si les données sont dans la liste des comptes 
-  // * si oui, renvoyer vers la page home avec un objet compte
-  // * si non, rester sur la même page
-
-  compteForm : FormGroup;
-  soumettre = false;
-
   constructor() { }
   
   ngOnInit(): void {}
-
-  /**
-   * Récupérer les données du formulaire
-   * @public
-   */
-  public onSubmit = () : void => {
-    this.soumettre = true;
-  }
 
 }
