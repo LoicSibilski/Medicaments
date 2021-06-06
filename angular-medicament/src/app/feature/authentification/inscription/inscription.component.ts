2ccf1ca35a851092bca28a21fd03deb9f4a331e5
Fabien <39435781+fabienbidault@users.noreply.github.com>
2021-06-06 21:30:10 +0200

    Ajout de la méthode pour créer un compte

--- angular-medicament/src/app/feature/authentification/inscription/inscription.component.ts
+++ angular-medicament/src/app/feature/authentification/inscription/inscription.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Compte } from '../../compte/models/compte';
+import { CompteService } from '../../compte/services/compte.service';
 
 @Component({
   selector: 'app-inscription',
@@ -15,81 +17,34 @@ import { Compte } from '../../compte/models/compte';
  */
 export class InscriptionComponent implements OnInit {
 
-  // TODO 
-  // matcher les deux mots de passe (ok)
-  // vérifier la réception des données du formulaire
-  // créer un objet compte et le stocker dans une liste
-  // rediriger vers la page connexion
-  // créer une alert : "compte créé"
-  // commenter le code
+  inscriptionForm : FormGroup;
 
-  compteForm: FormGroup;
-  soumettre = false;
+  compte : Compte = new Compte(0, "" , "", []);
+  compteurIdentifiant = 1;
 
-  /**
-   * @constructor
-   * @param formBuilder
-   */
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
-    let motDePasseRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[-+!*$@%_])([-+!*$@%_\w]{8,15})$/;
-    this.compteForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      motDePasse: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(15), Validators.pattern(motDePasseRegex)]],
-      confirmeMotDePasse: ['', Validators.required]
-    }, {
-      validator: this.mustMatch("motDePasse", "confirmeMotDePasse")
-    });
-  }
-
-  /**
-   * Récupérer un champs du formulaire
-   * @public
-   */
-  public get input() {
-    return this.compteForm.controls;
-  }
-
-  /**
-   * Récupérer les données du formulaire
-   * @public
-   */
-  public onSubmit = (): void => {
-    this.soumettre = true;
-    // Stop si le formulaire est invalide
-    console.log(this.compteForm.value);
-    if (this.compteForm.invalid) {
-      return;
+  constructor(
+    private service : CompteService, 
+    private fb : FormBuilder, 
+    private router : Router
+    ) { 
+      this.inscriptionForm = this.fb.group({
+        email : "",
+        motDePasse : "",
+        motDePasseConfirme : ""
+      })
     }
-    console.log(this.compteForm.value);
-  }
+
+  ngOnInit(): void {}
 
   /**
-   * Rénitialiser les inputs du formulaire
-   * @public
+   * Cette méthode permet de créer un compte
    */
-  public onReset = (): void => {
-    this.soumettre = false;
-    this.compteForm.reset();
-  }
-
-
-  public mustMatch(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup) => {
-      const control = formGroup.controls[controlName];
-      const matchingControl = formGroup.controls[matchingControlName];
-
-      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
-        return;
-      }
-
-      if (control.value !== matchingControl.value) {
-        matchingControl.setErrors({ mustMatch: true });
-      } else {
-        matchingControl.setErrors(null);
-      }
-
-    }
+  creerCompte = () => {
+    let email = this.inscriptionForm.value.email;
+    let motDePasse = this.inscriptionForm.value.motDePasse;
+    let nouveauCompte = new Compte(++this.compteurIdentifiant, email, motDePasse, []);
+    this.service.save(nouveauCompte);
+    console.log(this.service.listeComptes);
+    this.router.navigate(["/connexion"])
   }
 }
