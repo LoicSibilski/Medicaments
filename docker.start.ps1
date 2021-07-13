c8ea580f68717235cf0b4e62d74a8db42c331fd6
LoicSibilski <aeneas62@hotmail.fr>
2021-07-13 17:04:03 +0200

    Ajout de Kong et reafactorisation des fichiers

--- docker.start.ps1
+++ docker.start.ps1
@@ -1,1 +1,5 @@
-docker-compose -f .\docker-compose.yml -f .\docker-compose.supervision.yml up
+docker-compose  -f ./docker-compose/docker-compose.mongo.yml
+                -f ./docker-compose/docker-compose.elastic.yml
+                -f ./docker-compose/docker-compose.supervision.yml
+                -f ./docker-compose/docker-compose.yml
+                -f ./docker-compose/docker-compose.gateway up
