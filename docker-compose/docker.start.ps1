f27d347db8d809125d016859da217594c4605c5b
SamyMercier <56022459+SamyMercier@users.noreply.github.com>
2021-07-15 16:06:14 +0200

    encore des bugs mais moins

--- docker-compose/docker.start.ps1
+++ docker-compose/docker.start.ps1
@@ -1,2 +1,2 @@
-docker-compose -f docker-compose.elastic.yml -f docker-compose.mongo.yml -f docker-compose.supervision.yml -f docker-compose.gateway.yml up
+docker-compose -f ./docker-compose.elastic.yml -f ./docker-compose.supervision.yml -f ./docker-compose.gateway.yml -f ./docker-compose.yml up
 
