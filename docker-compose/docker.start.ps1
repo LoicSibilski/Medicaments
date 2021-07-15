3eeee584d9cfa7e35d82d81a76d3b0ba798e83e6
SamyMercier <56022459+SamyMercier@users.noreply.github.com>
2021-07-15 12:21:20 +0200

    mabite

--- docker-compose/docker.start.ps1
+++ docker-compose/docker.start.ps1
@@ -1,1 +1,2 @@
-docker-compose -f docker-compose.mongo.yml -f docker-compose.elastic.yml -f docker-compose.supervision.yml -f docker-compose.yml -f docker-compose.gateway.yml up
+docker-compose -f docker-compose.elastic.yml -f docker-compose.mongo.yml -f docker-compose.supervision.yml -f docker-compose.gateway.yml up
+
